bd298d342bfc86540fbf03c356c4048e71ea4450
kennedybabu <kennedynjogu8@gmail.com>
2022-08-09 17:38:26 +0300

    feature:get random nums in an array

--- src/App.js
+++ src/App.js
@@ -3,24 +3,40 @@ import Dice from "./components/Dice";
 
 
 function App() {
-  let num = 10
+  function allNewDice() {
+    
+    let arr = []
+    
+    for(let i = 1; i <= 10; i ++) {
+      let randomNum = Math.floor(Math.random() * 7)
+      if(randomNum === 0) {
+        return randomNum + 1
+      }
+
+      arr.push(randomNum)
+    }
+    console.log(arr)
+  }
   
   return (
     <div className="h-full w-full flex items-center justify-center">
        <main className="w-[360px] h-[379px] bg-[#0B2434] my-auto flex items-center justify-center top-1/2 translate-y-1/2">
-          <div className="w-[320px] h-[320px] bg-[#f5f5f5f5] rounded-[10px] flex items-center">
-              <div className="w-full h-[100px] px-[34px] grid grid-cols-5">
-                 <Dice />
-                 <Dice />
-                 <Dice />
-                 <Dice />
-                 <Dice />
-                 <Dice />
-                 <Dice />
-                 <Dice />
-                 <Dice />
-                 <Dice />
+          <div className="w-[320px] h-[320px] bg-[#f5f5f5f5] rounded-[10px] flex flex-col text-center items-center pt-[32px]">
+                <h1 className="text-[#2b283a] text-[25px] font-bold">Tenzies</h1>
+                <p className="text-[#4a4e74] text-[13px] max-w-[232px]">Roll until all dice are the same. Click each dice to freeze it at its current Value between rolls.</p>
+              <div className="w-full h-[100px] px-[34px] grid grid-cols-5 mt-[23px]">                
+                 <Dice value={1}/>
+                 <Dice value={3}/>
+                 <Dice value={1}/>
+                 <Dice value={6}/>
+                 <Dice value={3}/>
+                 <Dice value={4}/>
+                 <Dice value={2}/>
+                 <Dice value={6}/>
+                 <Dice value={3}/>
+                 <Dice value={1}/>
               </div>
+              <button onClick={allNewDice} className="w-[92px] h-[35px] bg-[#5035ff] mt-[10px] rounded-[4px] text-[#f5f5f5]">Roll</button>
           </div>
       </main>
     </div>
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Dice = () => {
+const Dice = (prop) => {
   return (
     <div className='h-[36px] w-[36px] rounded-[4px] dice flex items-center justify-center'>
-        <h2>1</h2>
+        <p className='text-[#2b283a] text-[21px]'>{prop.value}</p>
     </div>
   )
 }
