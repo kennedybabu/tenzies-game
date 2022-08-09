bf4822f22568ac3cf6bbcc46b05f96742fcad48c
kennedybabu <kennedynjogu8@gmail.com>
2022-08-09 16:59:21 +0300

    feature: create and render the Dice component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import Dice from "./components/Dice";
+
 
 
 function App() {
+  let num = 10
+  
   return (
     <div className="h-full w-full flex items-center justify-center">
        <main className="w-[360px] h-[379px] bg-[#0B2434] my-auto flex items-center justify-center top-1/2 translate-y-1/2">
-          <div className="w-[320px] h-[320px] bg-white rounded-[10px]">
-
+          <div className="w-[320px] h-[320px] bg-[#f5f5f5f5] rounded-[10px] flex items-center">
+              <div className="w-full h-[100px] px-[34px] grid grid-cols-5">
+                 <Dice />
+                 <Dice />
+                 <Dice />
+                 <Dice />
+                 <Dice />
+                 <Dice />
+                 <Dice />
+                 <Dice />
+                 <Dice />
+                 <Dice />
+              </div>
           </div>
       </main>
     </div>
---
+++ src/components/Dice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dice = () => {
+  return (
+    <div className='h-[36px] w-[36px] rounded-[4px] dice flex items-center justify-center'>
+        <h2>1</h2>
+    </div>
+  )
+}
+
+export default Dice
