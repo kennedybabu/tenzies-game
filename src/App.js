8fffc56a29c4b5cb2f071027780fa35e06fedd71
kennedybabu <kennedynjogu8@gmail.com>
2022-08-09 16:38:50 +0300

    feature: create and style the main element

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@
 
 function App() {
   return (
-    <div className="text-xl">
-      hey
+    <div className="h-full w-full flex items-center justify-center">
+       <main className="w-[360px] h-[379px] bg-[#0B2434] my-auto flex items-center justify-center top-1/2 translate-y-1/2">
+          <div className="w-[320px] h-[320px] bg-white rounded-[10px]">
+
+          </div>
+      </main>
     </div>
+   
   );
 }
 
